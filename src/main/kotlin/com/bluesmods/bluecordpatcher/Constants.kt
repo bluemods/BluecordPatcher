afd8b636fd66a8f9e5767d297f4a70b15224f431
Blue <blue@bluesmods.com>
2024-10-30 20:48:02 -0400

    Update APKTool to 2.10.0

--- src/main/kotlin/com/bluesmods/bluecordpatcher/Constants.kt
+++ src/main/kotlin/com/bluesmods/bluecordpatcher/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
 
     const val ANDROID_PLATFORM_TOOLS_VERSION = "r34.0.5"
     const val ANDROID_BUILD_TOOLS_VERSION = "r34-rc4"
-    const val APKTOOL_VERSION = "2.9.3"
+    const val APKTOOL_VERSION = "2.10.0"
     const val SMALI_VERSION = "2.5.2"
     const val BAKSMALI_VERSION = "2.5.2"
 }
